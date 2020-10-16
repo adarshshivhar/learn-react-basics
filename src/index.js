998a3f31c927a93b126e8bf0ec3b6146e70f5957
Adarsh Shivhare <adarshshivhare789@gmail.com>
2020-10-16 23:34:38 +0530

    JSX CSS

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 /**
- * * BOOK LIST Add CSS
+ * * BOOK LIST Add JSX-CSS
  */
-// CSS
+
 import './index.css';
 
 function BookList() {
@@ -21,16 +21,25 @@ function BookList() {
 }
 
 const Book = () => {
-    return(
-        <article className='book'>
-            <Image />
-            <Title />
-            <Author />
-        </article>
-    );
-}
-const Image = () => <img src="https://m.media-amazon.com/images/I/81V8qATf6xL._AC_UY327_FMwebp_QL65_.jpg" alt="" />;
-const Title = () => <h1>The Immortals Of Meluha (Hindi)</h1>;
+  return (
+    <article className='book'>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+const Image = () => (
+  <img
+    src='https://m.media-amazon.com/images/I/81V8qATf6xL._AC_UY327_FMwebp_QL65_.jpg'
+    alt=''
+  />
+);
+const Title = () => (
+  <h1 style={{ color: 'red', fontSize: '0.75rem', margin: '0.25rem' }}>
+    The Immortals Of Meluha (Hindi)
+  </h1>
+);
 const Author = () => <h4>by Amish</h4>;
 
 ReactDom.render(<BookList />, document.getElementById('root'));
