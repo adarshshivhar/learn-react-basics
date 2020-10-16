73722052d260f56bb8985f7d2bf1808d5e00b7f4
Adarsh Shivhare <adarshshivhare789@gmail.com>
2020-10-17 00:16:52 +0530

    Props

--- src/index.js
+++ src/index.js
@@ -2,31 +2,47 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 /**
- * * BOOK LIST Add JSX-Javascript
+ * * BOOK LIST PROPS
  */
 
 import './index.css';
 
+const firstBook = {
+    img: 'https://m.media-amazon.com/images/I/81V8qATf6xL._AC_UY327_FMwebp_QL65_.jpg',
+  title: 'The Immortals Of Meluha (Hindi)',
+ author: 'by Amish'
+}
+
+const secondBook = {
+  img: 'https://m.media-amazon.com/images/I/9138VXjBfPL._AC_UY327_FMwebp_QL65_.jpg',
+  title: 'The Power of Your Subconscious Mind (DELUXE HARDBOUND EDITION)',
+  author: 'by Dr. Joseph Murphy',
+};
+
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const author = 'by Amish';
-const Book = () => {
-  const title = 'The Immortals Of Meluha (Hindi)';
+const Book = (props) => {
+  console.log(props);
   return (
     <article className='book'>
-      <img
-        src='https://m.media-amazon.com/images/I/81V8qATf6xL._AC_UY327_FMwebp_QL65_.jpg'
-        alt=''
-      />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-      <p>{6 + 6}</p>
+      <img src={props.img} alt='' />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
