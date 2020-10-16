638b95784d40ec6ee5f9ab4724002deda9dd2bac
Adarsh Shivhare <adarshshivhare789@gmail.com>
2020-10-17 01:36:36 +0530

    Import and Export Statements

---
+++ src/books.js
@@ -0,0 +1,23 @@
+export const data = [
+  {
+    id: 1,
+    img:
+      'https://m.media-amazon.com/images/I/81V8qATf6xL._AC_UY327_FMwebp_QL65_.jpg',
+    title: 'The Immortals Of Meluha',
+    author: 'by Amish',
+  },
+  {
+    id: 2,
+    img:
+      'https://m.media-amazon.com/images/I/9138VXjBfPL._AC_UY327_FMwebp_QL65_.jpg',
+    title: 'The Power of Your Subconscious Mind',
+    author: 'by Dr. Joseph Murphy',
+  },
+  {
+    id: 3,
+    img:
+      'https://images-eu.ssl-images-amazon.com/images/I/61Nta+jYOPL._AC_SX184_.jpg',
+    title: 'Tenali Raman',
+    author: 'by Maple Press',
+  },
+];
--- src/index.js
+++ src/index.js
@@ -6,65 +6,22 @@ import ReactDom from 'react-dom';
  */
 
 import './index.css';
-const Books = [
-  {
-    id: 1,
-    img:
-      'https://m.media-amazon.com/images/I/81V8qATf6xL._AC_UY327_FMwebp_QL65_.jpg',
-    title: 'The Immortals Of Meluha',
-    author: 'by Amish',
-  },
-  {
-    id: 2,
-    img:
-      'https://m.media-amazon.com/images/I/9138VXjBfPL._AC_UY327_FMwebp_QL65_.jpg',
-    title: 'The Power of Your Subconscious Mind',
-    author: 'by Dr. Joseph Murphy',
-  },
-  {
-    id: 3,
-    img:
-      'https://images-eu.ssl-images-amazon.com/images/I/61Nta+jYOPL._AC_SX184_.jpg',
-    title: 'Tenali Raman',
-    author: 'by Maple Press',
-  },
-];
+
+//Named Export it should be same
+import { data } from './books';
+//Default Export it should not be same as we declare in creating function
+import SpecificBook from './Book';
 
 function BookList() {
   return (
     <section className='booklist'>
-      {Books.map((book, index) => {
+      {data.map((book, index) => {
         //const { img, title, author } = book;
         // return <Book key={index} img={img} title={title} author={author} />;
-        return <Book key={book.id} {...book} />;
+        return <SpecificBook key={book.id} {...book} />;
       })}
     </section>
   );
 }
-
-const Book = (props) => {
-  const { img, title, author } = props;
-
-  const clickHandler = (e) => {
-      console.log(e);
-      console.log(e.target);
-      alert('hello world');
-  }
-  const complexExample = (author) => {
-    console.log(author);
-  }
-  return (
-    <article className='book' onMouseOver={() => {
-        console.log(title);
-    }}>
-      <img src={img} alt='' />
-      <h1 onClick={() => console.log(title)}>{title}</h1>
-      <h4>{author}</h4>
-      <button type='button' onClick={clickHandler}>reference example</button>
-      <br />
-      <button type='button' onClick={() => complexExample(author)}>more complex example</button>
-    </article>
-  );
-};
 
 ReactDom.render(<BookList />, document.getElementById('root'));
