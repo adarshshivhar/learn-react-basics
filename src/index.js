7532b50905a8a2aeb8ef7c3c34ebab290440d901
Adarsh Shivhare <adarshshivhare789@gmail.com>
2020-10-17 00:56:11 +0530

    Props List

--- src/index.js
+++ src/index.js
@@ -2,55 +2,50 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 /**
- * * BOOK PROPS CHILDREN
+ * * BOOK LIST
  */
 
 import './index.css';
-
-const firstBook = {
-  img:
-    'https://m.media-amazon.com/images/I/81V8qATf6xL._AC_UY327_FMwebp_QL65_.jpg',
-  title: 'The Immortals Of Meluha',
-  author: 'by Amish',
-};
-
-const secondBook = {
-  img:
-    'https://m.media-amazon.com/images/I/9138VXjBfPL._AC_UY327_FMwebp_QL65_.jpg',
-  title: 'The Power of Your Subconscious Mind',
-  author: 'by Dr. Joseph Murphy',
-};
+const Books = [
+  {
+    img:
+      'https://m.media-amazon.com/images/I/81V8qATf6xL._AC_UY327_FMwebp_QL65_.jpg',
+    title: 'The Immortals Of Meluha',
+    author: 'by Amish',
+  },
+  {
+    img:
+      'https://m.media-amazon.com/images/I/9138VXjBfPL._AC_UY327_FMwebp_QL65_.jpg',
+    title: 'The Power of Your Subconscious Mind',
+    author: 'by Dr. Joseph Murphy',
+  },
+  {
+    img:
+      'https://images-eu.ssl-images-amazon.com/images/I/61Nta+jYOPL._AC_SX184_.jpg',
+    title: 'Tenali Raman',
+    author: 'by Maple Press',
+  },
+];
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
-          enim quam minima laudantium, fuga accusamus?
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {Books.map((book, index) => {
+        //const { img, title, author } = book;
+        // return <Book key={index} img={img} title={title} author={author} />;
+        return <Book key={index} book={book} />;
+      })}
     </section>
   );
 }
 
 const Book = (props) => {
-  const {img, title, author} = props 
+  const { img, title, author } = props.book;
   return (
     <article className='book'>
       <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {props.children}
     </article>
   );
 };
