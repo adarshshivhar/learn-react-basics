c13828375fed380e0c5f87bd2e5ba74753dc0ccd
Adarsh Shivhare <adarshshivhare789@gmail.com>
2020-10-17 01:01:37 +0530

    Another way to get book list using spread operator

--- src/index.js
+++ src/index.js
@@ -8,18 +8,21 @@ import ReactDom from 'react-dom';
 import './index.css';
 const Books = [
   {
+    id: 1,
     img:
       'https://m.media-amazon.com/images/I/81V8qATf6xL._AC_UY327_FMwebp_QL65_.jpg',
     title: 'The Immortals Of Meluha',
     author: 'by Amish',
   },
   {
+    id: 2,
     img:
       'https://m.media-amazon.com/images/I/9138VXjBfPL._AC_UY327_FMwebp_QL65_.jpg',
     title: 'The Power of Your Subconscious Mind',
     author: 'by Dr. Joseph Murphy',
   },
   {
+    id: 3,
     img:
       'https://images-eu.ssl-images-amazon.com/images/I/61Nta+jYOPL._AC_SX184_.jpg',
     title: 'Tenali Raman',
@@ -33,14 +36,14 @@ function BookList() {
       {Books.map((book, index) => {
         //const { img, title, author } = book;
         // return <Book key={index} img={img} title={title} author={author} />;
-        return <Book key={index} book={book} />;
+        return <Book key={book.id} {...book} />;
       })}
     </section>
   );
 }
 
 const Book = (props) => {
-  const { img, title, author } = props.book;
+  const { img, title, author } = props;
   return (
     <article className='book'>
       <img src={img} alt='' />
